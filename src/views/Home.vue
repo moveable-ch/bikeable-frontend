28c0b34b619f6ef957f1ef8ca7dbcfad2e31b59f
Samuel Alder <samuel.alder@gmail.com>
2017-09-26 14:39:57 +0200

    newsletter

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -60,12 +60,45 @@
       </div>
     </div>
 
-    <!--<div class="home__newsletter">
+    <div class="home__newsletter">
       <div class="container">
         <h2>Newsletter</h2>
+        <p>Melde dich hier für unseren Newsletter an und erfahre regelmässig, was bei bikeable läuft.<br>Kein Spam imfall!</p>
+        <!-- Begin MailChimp Signup Form -->
+        <div id="mc_embed_signup">
+          <form action="//bikeable.us15.list-manage.com/subscribe/post?u=5a9614abf208b7faae96233ff&amp;id=6d62b30edc" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+            <div id="mc_embed_signup_scroll">
+              <div class="mc-field-group">
+                <label>
+                  <span>Email Adresse</span>
+                  <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+                </label>
+              </div>
+              <div class="mc-field-group">
+                <label>
+                  <span>Vorname</span>
+                  <input type="text" value="" name="FNAME" class="" id="mce-FNAME">
+                </label>
+              </div>
+              <div class="mc-field-group">
+                <label>
+                  <span>Nachname</span>
+                  <input type="text" value="" name="LNAME" class="" id="mce-LNAME">
+                </label>
+              </div>
+              <div id="mce-responses" class="clear">
+                <div class="response" id="mce-error-response" style="display:none"></div>
+                <div class="response" id="mce-success-response" style="display:none"></div>
+              </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+              <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_5a9614abf208b7faae96233ff_6d62b30edc" tabindex="-1" value=""></div>
+              <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn"></div>
+            </div>
+          </form>
+        </div>
 
+        <!--End mc_embed_signup-->
       </div>
-    </div>-->
+    </div>
   </div>
 </template>
 
@@ -191,6 +224,13 @@ export default {
 @import '../styles/helpers';
 
 .home {
+
+  &__newsletter {
+
+    p {
+      margin-bottom: 2rem;
+    }
+  }
 
   &__news {
     margin: 3rem 0;

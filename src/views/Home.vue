f121e7b03a3288ec410a580a5f6601353b3beea2
Samuel Alder <samuel.alder@gmail.com>
2018-05-19 21:41:03 +0200

    Updated mailchimp form

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -68,7 +68,7 @@
         <p>Melde dich hier für unseren Newsletter an und erfahre regelmässig, was bei bikeable läuft.<br>Kein Spam imfall!</p>
         <!-- Begin MailChimp Signup Form -->
         <div id="mc_embed_signup">
-          <form action="//bikeable.us15.list-manage.com/subscribe/post?u=5a9614abf208b7faae96233ff&amp;id=6d62b30edc" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+          <form action="https://bikeable.us15.list-manage.com/subscribe/post?u=5a9614abf208b7faae96233ff&amp;id=1df02df80c" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
             <div id="mc_embed_signup_scroll">
               <div class="mc-field-group">
                 <label>

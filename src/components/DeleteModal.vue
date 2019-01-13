6207eed32c94963a7a54c73c30fdcd1d59c564a7
Marc Gschwend <gschwend@ubique.ch>
2019-01-13 16:03:15 +0100

    #61: Go back to last view, after deleting a post. Update node-sass to 4.11 to work on my computer...

--- src/components/DeleteModal.vue
+++ src/components/DeleteModal.vue
@@ -38,7 +38,9 @@ export default {
           id: this.entryId
       })
       .then(() => {
-          this.$router.push('/');
+        window.history.length > 1
+        ? this.$router.go(-1)
+        : this.$router.push('/');
         });
     }
   }

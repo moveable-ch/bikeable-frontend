7a4964ba59d4761f86ece82293717b7f953a8b74
Marc Gschwend <gschwend@ubique.ch>
2020-04-14 18:21:04 +0200

    Adds tags.js for store

---
+++ src/api/tags.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+var config = require('../../config');
+
+
+
+export default {
+  getTags() {
+    let url =  process.env.BACKEND_URL + '/api/v1/tags';
+
+    return new Promise((resolve, reject) => {
+      axios.get(url)
+        .then(response => {
+          resolve(response.data);
+        }, error => {
+          console.log(error.request);
+          if(!error.request.response) reject('');
+          let msg = JSON.parse(error.request.response);
+          reject(msg.message);
+        });
+    });
+  }
+
+  
+}

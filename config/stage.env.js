ca88088f7582f6f3c970b6cbef3b34efb3a3ce7d
Samuel Alder <samuel.alder@gmail.com>
2020-04-28 19:42:58 +0200

    Initial vue-cli commit

--- config/stage.env.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  NODE_ENV: '"staging"',
-  BACKEND_URL: '\'https://stagingbackend.bikeable.ch\''
-}
--- src/main.js
+++ src/main.js
@@ -1,39 +1,12 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import SocialSharing from 'vue-social-sharing'
-import VueI18n from 'vue-i18n'
-import App from './App'
+import App from './App.vue'
 import router from './router'
-import VueHead from 'vue-head'
-import store from '@/store'
-import locale from './locale.json'
-import * as Sentry from '@sentry/browser'
-import * as Integrations from '@sentry/integrations'
+import store from './store'
 
-Vue.use(VueHead);
-Vue.use(SocialSharing);
-Vue.use(VueI18n);
-
-/* sentry setup */
-if(process.env.NODE_ENV === 'production') {
-  Sentry.init({
-    dsn: 'https://2e16e9a0244442a38099e186eaa64bfa@sentry.io/1782729',
-    integrations: [new Integrations.Vue({Vue, attachProps: true})],
-  });
-}
-/* */
-
-const i18n = new VueI18n({
-  locale: 'de',
-  messages: locale
-})
+Vue.config.productionTip = false
 
 new Vue({
-  el: '#app',
-  store,
   router,
-  i18n,
-  template: '<App/>',
-  components: { App }
-})
+  store,
+  render: h => h(App)
+}).$mount('#app')

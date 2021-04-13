6f2bf8526c455f2c6dd5fff6e793954af3d99a98
Samuel Alder <samuel.alder@gmail.com>
2021-04-13 16:51:41 +0200

    Fixed news layout

--- src/views/News.vue
+++ src/views/News.vue
@@ -5,13 +5,27 @@
     <div class="container" v-if="news">
       <h1>News</h1>
       <div class="newsarchive__container">
-        <div class="newsarchive__item" v-for="article in news">
-          <router-link class="newsarchive__imagebox" :to="'/news/' + article.id"><img class="newsarchive__image" :src="article.image"></router-link>
+        <div
+          class="newsarchive__item"
+          v-for="article in news"
+          :key="article.id"
+        >
+          <router-link class="newsarchive__imagebox" :to="'/news/' + article.id"
+            ><img class="newsarchive__image" :src="article.image"
+          /></router-link>
           <div class="newsarchive__content">
-            <h3><router-link :to="'/news/' + article.id">{{ article.title }}</router-link></h3>
             <span class="date">{{ article.date }}</span>
+            <h3>
+              <router-link :to="'/news/' + article.id">{{
+                article.title
+              }}</router-link>
+            </h3>
             <p>{{ article.abstract }}</p>
-            <router-link class="newsarchive__more" :to="'/news/' + article.id">{{ $t('home.more') }}</router-link>
+            <router-link
+              class="newsarchive__more"
+              :to="'/news/' + article.id"
+              >{{ $t("home.more") }}</router-link
+            >
           </div>
         </div>
       </div>
@@ -20,79 +34,82 @@
 </template>
 
 <script>
-
-import Prismic from 'prismic.io';
+import Prismic from "prismic.io";
 
 export default {
-  name: 'v-news',
-  data () {
+  name: "v-news",
+  data() {
     return {
-      news: null
-    }
+      news: null,
+    };
   },
   mounted() {
     // TODO: Move to Store
-    this.$store.commit('LOAD_START');
+    this.$store.commit("LOAD_START");
 
-    this.getData().then(data => {
-      this.$store.commit('LOAD_FINISH');
+    this.getData().then((data) => {
+      this.$store.commit("LOAD_FINISH");
       this.news = data;
     });
   },
   computed: {
     prismicLang() {
       return this.$store.getters.prismicLang;
-    }
+    },
   },
   watch: {
-    'prismicLang' (to, from) {
-
+    prismicLang(to, from) {
       // TODO: Move to Store
-      this.$store.commit('LOAD_START');
+      this.$store.commit("LOAD_START");
 
-      this.getData().then(data => {
-        this.$store.commit('LOAD_FINISH');
+      this.getData().then((data) => {
+        this.$store.commit("LOAD_FINISH");
         this.news = data;
       });
-    }
+    },
   },
   methods: {
     getData() {
-      return Prismic.api("https://bikeable.prismic.io/api").then((api) => {
-        return api.query(
-          Prismic.Predicates.at('document.type', 'news'),
-          {
-            orderings: '[document.first_publication_date desc]',
-            lang: this.prismicLang
+      return Prismic.api("https://bikeable.prismic.io/api")
+        .then((api) => {
+          return api.query(Prismic.Predicates.at("document.type", "news"), {
+            orderings: "[document.first_publication_date desc]",
+            lang: this.prismicLang,
+          });
+        })
+        .then(
+          function (payload) {
+            const y = [];
+            y.questions = payload.results.map((x) => {
+              const z = {};
+              z.id = x.id;
+              let date = new Date(x.firstPublicationDate);
+              z.date =
+                date.getDate() +
+                "." +
+                (date.getMonth() + 1) +
+                "." +
+                date.getFullYear();
+              z.title = x.getText("news.news_title");
+              z.abstract = x.getText("news.news_abstract");
+              z.image = x.getImage("news.news_image").views.preview.url;
+              y.push(z);
+            });
+            return y;
+          },
+          function (err) {
+            console.log("Something went wrong: ", err);
           }
         );
-      }).then(function(payload) {
-        const y = [];
-        y.questions = payload.results.map((x) => {
-          const z = {};
-          z.id = x.id;
-          let date = new Date(x.firstPublicationDate);
-          z.date = date.getDate() + '.' + (date.getMonth()+1) + '.' + date.getFullYear();
-          z.title = x.getText('news.news_title');
-          z.abstract = x.getText('news.news_abstract');
-          z.image = x.getImage('news.news_image').views.preview.url;
-          y.push(z);
-        });
-        return y;
-      }, function(err) {
-        console.log("Something went wrong: ", err);
-      });
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-
-@import '../styles/helpers';
+@import "../styles/helpers";
 
 .newsarchive {
-
   &__container {
     margin-top: 2rem;
   }
@@ -103,13 +120,14 @@ export default {
 
     @include tablet() {
       max-width: 850px;
+      margin: 3rem 0;
     }
   }
 
   &__more {
     display: inline-block;
-    font-size: .9rem;
-    margin-top: .5rem;
+    font-size: 0.9rem;
+    margin-top: 0.75rem;
   }
 
   &__content {
@@ -118,8 +136,9 @@ export default {
 
     h3 {
       line-height: 1.1;
-      font-size: 1.25rem;
+      font-size: 1.3rem;
       margin-bottom: 0;
+      margin: .5rem 0;
 
       a {
         text-decoration: none;
@@ -127,28 +146,31 @@ export default {
     }
 
     p {
-      margin-top: .5rem;
-      font-size: .9rem;
+      margin-top: 0.5rem;
+      margin-bottom: 0;
+      font-size: 0.9rem;
     }
 
     .date {
-      font-size: .9rem;
-      color: #aaa;
+      font-size: 0.8rem;
+      opacity: .5;
     }
 
     @include tablet() {
       padding-left: 1.5rem;
-      padding-top: .5rem;
+      padding-top: 0;
     }
   }
 
   &__imagebox {
     display: block;
-    border-radius: 4px;
+    border-radius: 8px;
     overflow: hidden;
-    width: 80px;
-    height: 80px;
-    box-shadow: 0 5px 15px -5px rgba($c-black, .2);
+    width: 4rem;
+    height: 4rem;
+    flex-shrink: 0;
+    background-color: #fff;
+    box-shadow: 0 5px 15px -5px rgba($c-black, 0.2);
 
     @include tablet() {
       width: 200px;
@@ -160,8 +182,7 @@ export default {
     width: 100%;
     height: 100% !important;
     flex-shrink: 0;
+    object-fit: cover;
   }
 }
-
-
 </style>

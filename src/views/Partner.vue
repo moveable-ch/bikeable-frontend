5bb4694223b61c4f36d7d6af48667126858a2ce1
Samuel Alder <samuel.alder@gmail.com>
2023-02-02 13:02:00 +0100

    Improve partner layout

--- src/views/Partner.vue
+++ src/views/Partner.vue
@@ -3,113 +3,135 @@
 <template>
   <div class="contentpage partner">
     <div class="container">
-      <h1>{{ $t('partner.partner') }}</h1>
+      <h1>{{ $t("partner.partner") }}</h1>
       <div class="partners" v-if="partners">
-        <a target="_blank" v-for="(partner, index) in partners" :key="index" :href="partner.website.url()" class="partner__item">
-          <span class="partner__item__logo" :style="'background-image:url(' + partner.logo + ')'"></span>
+        <a
+          target="_blank"
+          v-for="(partner, index) in partners"
+          :key="index"
+          :href="partner.website.url()"
+          class="partner__item"
+        >
+          <span
+            class="partner__item__logo"
+            :style="'background-image:url(' + partner.logo + ')'"
+          ></span>
         </a>
       </div>
 
       <ul class="sponsors" v-if="sponsors">
-        <li v-for="(sponsor, index) in sponsors" :key="index"><a target="_blank" :href="sponsor.website.url()">{{ sponsor.name }}</a></li>
+        <li v-for="(sponsor, index) in sponsors" :key="index">
+          <a target="_blank" :href="sponsor.website.url()">{{
+            sponsor.name
+          }}</a>
+        </li>
       </ul>
 
       <div class="provelo-map">
-        <h3>{{ $t('partner.proveloregions') }}:</h3>
-        <iframe src="https://html.bikeable.ch/provelo-map.html" frameborder="0"></iframe>
+        <h3>{{ $t("partner.proveloregions") }}:</h3>
+        <iframe
+          src="https://html.bikeable.ch/provelo-map.html"
+          frameborder="0"
+        ></iframe>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-
-import Prismic from 'prismic.io';
+import Prismic from "prismic.io";
 
 export default {
-  name: 'v-partner',
-  data () {
+  name: "v-partner",
+  data() {
     return {
       partners: null,
-      sponsors: null
-    }
+      sponsors: null,
+    };
   },
   mounted() {
-
     // TODO: Move to Store
-    this.$store.commit('LOAD_START');
+    this.$store.commit("LOAD_START");
 
-    this.getPartners().then(partners => {
+    this.getPartners().then((partners) => {
       this.partners = partners;
-      this.$store.commit('LOAD_FINISH');
+      this.$store.commit("LOAD_FINISH");
     });
 
-    this.getSponsors().then(sponsors => {
+    this.getSponsors().then((sponsors) => {
       this.sponsors = sponsors;
     });
   },
   methods: {
     getPartners() {
-      return Prismic.api("https://bikeable.prismic.io/api").then(function(api) {
-        return api.query(
-          Prismic.Predicates.at('document.type', 'partner'),
-          { lang: '*' }
+      return Prismic.api("https://bikeable.prismic.io/api")
+        .then(function (api) {
+          return api.query(Prismic.Predicates.at("document.type", "partner"), {
+            lang: "*",
+          });
+        })
+        .then(
+          function (payload) {
+            return payload.results.map((x) => {
+              const y = {};
+              y.name = x.getText("partner.name");
+              y.logo = x.getImage("partner.logo").url;
+              y.website = x.getLink("partner.website");
+              return y;
+            });
+          },
+          function (err) {
+            console.log("Something went wrong: ", err);
+          }
         );
-      }).then(function(payload) {
-        return payload.results.map((x) => {
-          const y = {};
-          y.name = x.getText('partner.name');
-          y.logo = x.getImage('partner.logo').url;
-          y.website = x.getLink('partner.website');
-          return y;
-        });
-      }, function(err) {
-        console.log("Something went wrong: ", err);
-      });
     },
     getSponsors() {
-      return Prismic.api("https://bikeable.prismic.io/api").then(function(api) {
-        return api.query(
-          Prismic.Predicates.at('document.type', 'sponsors'),
-          { lang: '*' }
+      return Prismic.api("https://bikeable.prismic.io/api")
+        .then(function (api) {
+          return api.query(Prismic.Predicates.at("document.type", "sponsors"), {
+            lang: "*",
+          });
+        })
+        .then(
+          function (payload) {
+            return payload.results.map((x) => {
+              const y = {};
+              y.name = x.getText("sponsors.name");
+              y.website = x.getLink("sponsors.website");
+              return y;
+            });
+          },
+          function (err) {
+            console.log("Something went wrong: ", err);
+          }
         );
-      }).then(function(payload) {
-        return payload.results.map((x) => {
-          const y = {};
-          y.name = x.getText('sponsors.name');
-          y.website = x.getLink('sponsors.website');
-          return y;
-        });
-      }, function(err) {
-        console.log("Something went wrong: ", err);
-      });
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-
-@import '../styles/helpers';
+@import "../styles/helpers";
 
 .partner {
-
   .partners {
     width: 100%;
-    display: flex;
-    flex-wrap: wrap;
+    display: grid;
+    grid-template-columns: repeat(12, 1fr);
+    gap: 1rem;
     margin: 3rem 0;
   }
   &__item {
+    grid-column: span 6;
     display: block;
     width: 100%;
-    margin-bottom: 2.6%;
-    height: 10rem;
-    background-color: #fff;
+    aspect-ratio: 3/2;
+    height: auto;
+    background-color: rgba(#fff, 0.8);
     box-sizing: border-box;
     border: 2px solid #eee;
-    padding: 2rem;
-    transition: .1s border-color;
+    padding: 1.5rem;
+    border-radius: 4px;
 
     &__logo {
       display: block;
@@ -125,36 +147,46 @@ export default {
     }
 
     @include tablet() {
-      width: 32%;
-      margin-right: 2%;
-
-      &:nth-child(3n) {
-        margin-right: 0;
-      }
+      grid-column: span 4;
+    }
+    @include desktop() {
+      grid-column: span 3;
     }
   }
   ul.sponsors {
     margin: 0;
     padding: 0;
-    display: flex;
+    display: grid;
+    gap: 1rem;
+    grid-template-columns: repeat(12, 1fr);
     flex-wrap: wrap;
     font-family: $f-body;
     font-weight: 300;
     max-width: none;
+    align-items: flex-start;
 
     li {
-      padding: 0 1rem;
+      display: flex;
+      // justify-content: center;
+      padding: 0;
+      margin: 0;
       box-sizing: border-box;
       width: 100%;
       text-align: center;
+      grid-column: span 6;
 
       a {
+        display: inline-block;
         color: $c-main;
-        font-size: 1.25rem;
+        font-size: 1rem;
+        background-color: #fff;
+        padding: 0.5rem 1rem;
+        border-radius: 4px;
+        border: 2px solid #eee;
         text-decoration: none;
 
         &:hover {
-          color: $c-highlight;
+          border-color: $c-main;
         }
       }
       &::before {
@@ -162,7 +194,10 @@ export default {
       }
 
       @include tablet() {
-        width: 33.3%;
+        grid-column: span 4;
+      }
+      @include desktop() {
+        grid-column: span 3;
       }
     }
   }
@@ -183,6 +218,4 @@ export default {
     }
   }
 }
-
-
 </style>

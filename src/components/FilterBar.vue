eb33c96e4402e466d7c5da0f1ce23c14f127440a
Marc Gschwend <mgschwend>
2023-06-01 09:20:55 +0200

    Add fixed spot filter to map

--- src/components/FilterBar.vue
+++ src/components/FilterBar.vue
@@ -19,7 +19,7 @@
           <option value="null">{{ $t("filterbar.alltypes") }}</option>
           <option value="fame">Fame</option>
           <option value="shame">Shame</option>
-          <!--<option value="fixed">Fixed</option>-->
+          <option value="fixed">Fixed</option>
         </select>
       </div>
     </div>
